450e222f6873fcef00be94614c127407cd2e44d6
Nicolas <nicolas.beltrao69@gmail.com>
2022-02-23 09:59:33 -0300

    .

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -5,16 +5,16 @@ const administrator = false;
 
 app.delete("/:id/productos/:id_prod", (req, res, next) => {
 
-  console.log("Remove product!");
+  console.log("Remover");
   const { id, id_prod } = req.params;
   console.log(`Intentando remover producto ${id_prod} del carrito ${id} `);
 
-  const fscart = new fs_carrito();
+  const fscart = new carrinho(); 
   const resultado = await fscart.removeProductToCart(id, id_prod);
 
   res.status(200).json({
     code: 200,
-    message: `Estamos laburando para vos! ${resultado}.`,
+    message: `${resultado}.`,
   });
 })
 
@@ -24,17 +24,17 @@ app.post("/:id/productos", (req, res, next) => {
   const { id } = req.params;
   const { idproducto } = req.body;
 
-  const fscart = new fs_carrito();
+  const fscart = new carrinho(); 
   const resultado = await fscart.addProductToCart(id, idproducto);
   if (resultado) {
     res.status(200).json({
       code: 200,
-      message: `Se agrego el producto al carrito de forma correcta.`,
+      message: `Agregado`,
     });
   } else {
     res.status(400).json({
       code: 400,
-      message: `No se pudo agregar.`,
+      message: `Error`,
     });
   }
 })
@@ -44,7 +44,7 @@ app.get("/:id/productos", (req, res, next) => {
 
   try {
     const { id } = req.params;
-    const fscart = new fs_carrito(); 
+    const fscart = new carrinho(); //
     const productosEnCarro = await fscart.showProductsOfCart(id);
 
     if (productosEnCarro) {
@@ -53,16 +53,16 @@ app.get("/:id/productos", (req, res, next) => {
         productosEnCarro,
       });
     } else {
-      throw "No hay productos en este carrito!";
+      throw "No hay productos";
     }
   } catch (error) {
-    if (error === "No hay productos en este carrito!") {
+    if (error === "No hay productos") {
       return res.status(400).json({ code: 400, message: error });
     } else {
-      console.log("Error inesperado:", error);
+      console.log("Error", error);
       return res
         .status(400)
-        .json({ code: 400, message: "Intento de obtener productos fallido." });
+        .json({ code: 400, message: "Error" });
     }
   }
 
@@ -76,19 +76,19 @@ app.delete("/:id", (req, res, next) => {
   try {
     const { id } = req.params;
 
-    const fscart = new fs_carrito(); 
+    const fscart = new carrinho(); //
     const resultado = await fscart.deleteById(id);
     if (resultado === 400) {
       throw resultado;
     } else {
       return res
         .status(200)
-        .json({ code: 200, message: "Carrito borrado con exito!" });
+        .json({ code: 200, message: "Borrado" });
     }
   } catch (error) {
     return res
       .status(400)
-      .json({ code: 400, message: "No hay ningun carrito con esa ID" });
+      .json({ code: 400, message: "No existe esa ID" });
   }
 
 })
@@ -111,16 +111,16 @@ app.post("/:id/productos", (req, res, next) => {
             productos,
           });
         } else {
-          throw "No hay productos en este carrito!";
+          throw "No hay productos";
         }
       } catch (error) {
-        if (error === "No hay productos en este carrito!") {
+        if (error === "No hay productos") {
           return res.status(400).json({ code: 400, message: error });
         } else {
-          console.log("Error inesperado:", error);
+          console.log("Error", error);
           return res
             .status(400)
-            .json({ code: 400, message: "Intento de obtener productos fallido." });
+            .json({ code: 400, message: "Error" });
         }
       }
 
